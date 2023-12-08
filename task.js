689abd988011df0dfa13e2fed0dc2906da4ae9db
notRaihan <muhraihan185@gmail.com>
2023-12-08 10:16:39 +0700

    feat: task pertemuan 10

---
+++ task.js
@@ -0,0 +1,38 @@
+/**
+ * Fungsi untuk menampilkan hasil download
+ * @param {string} result - Nama file yang didownload
+ */
+const showDownload = (result) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`Download selesai \nHasil Download: ${result}`);
+        }, 2000);
+    });
+};
+
+/**
+ * Fungsi untuk download file
+ * @param {function} callback - Function callback show
+ */
+const download = (callShowDownload) => {
+    return new Promise((resolve) => {
+        setTimeout(function () {
+            resolve(callShowDownload("windows-10.exe"));
+        }, 3000);
+    });
+};
+
+download(showDownload)
+    .then((result) => {
+        console.log(result);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+/**
+ * TODO:
+ * - Refactor callback ke Promise atau Async Await - done
+ * - Refactor function ke ES6 Arrow Function - done
+ * - Refactor string ke ES6 Template Literals - done
+ */

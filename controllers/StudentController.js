cb83ff18b51b007e7d592ca6713a53a0714474ca
notRaihan <muhraihan185@gmail.com>
2023-12-15 10:00:34 +0700

    feat: finish task 11 students with array

--- controllers/StudentController.js
+++ controllers/StudentController.js
@@ -1,48 +1,75 @@
+// TODO 3: Import data students dari folder data/students.js
+const students = require("../data/students");
+
+// Membuat Class StudentController
 class StudentController {
     index(req, res) {
+        // TODO 4: Tampilkan data students
         const data = {
-            status: "success",
-            code: 200,
-            message: "Menampilkan data student",
-            data: [],
+            message: "Show All data students",
+            students: students,
         };
+
         res.json(data);
     }
 
     store(req, res) {
-        const { name } = req.body;
+        // TODO 5: Tambahkan data students
         const data = {
-            status: "success",
-            code: 200,
-            message: `Menambahkan data student ${name}`,
-            data: [],
+            message: "Create data students",
+            students: students,
         };
+
+        const { name } = req.body;
+
+        students.push({
+            id: students.length + 1,
+            name: name,
+        });
+
         res.json(data);
     }
 
     update(req, res) {
+        // TODO 6: Update data students
+        const data = {
+            message: "Update data students",
+            students: students,
+        };
+
         const { id } = req.params;
         const { name } = req.body;
 
-        const data = {
-            status: "success",
-            code: 200,
-            message: `Mengubah data student id: ${id}, name: ${name}`,
-            data: [],
-        };
+        students.forEach((student) => {
+            if (student.id == id) {
+                student.name = name;
+            }
+        });
+
         res.json(data);
     }
 
     destroy(req, res) {
-        const { id } = req.params;
+        // TODO 7: Hapus data students
         const data = {
-            status: "success",
-            code: 200,
-            message: `Menghapus data student id: ${id}`,
-            data: [],
+            message: "Delete data students",
+            students: students,
         };
+
+        const { id } = req.params;
+
+        students.forEach((student, index) => {
+            if (student.id == id) {
+                students.splice(index, 1);
+            }
+        });
+
         res.json(data);
     }
 }
 
-module.exports = new StudentController();
+// Membuat object StudentController
+const object = new StudentController();
+
+// Export object StudentController
+module.exports = object;
---
+++ data/students.js
@@ -0,0 +1,18 @@
+// TODO 1: Buat data students
+const students = [
+    {
+        id: 1,
+        name: "John Doe",
+    },
+    {
+        id: 2,
+        name: "Jane Doe",
+    },
+    {
+        id: 3,
+        name: "Foo Bar",
+    },
+];
+
+// TODO 2: export data students
+module.exports = students;
